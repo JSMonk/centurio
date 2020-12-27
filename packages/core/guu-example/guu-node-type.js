37e034071330cf0f6118fb4115693772a73f363c
Artem Kobzar <dendishiz0@gmail.com>
2020-12-27 14:25:33 +0200

    chore: remove compilation files from repository.

--- packages/core/guu-example/guu-node-type.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.GuuASTNodeType = void 0;
-var GuuASTNodeType;
-(function (GuuASTNodeType) {
-    GuuASTNodeType["ASSIGNMENT"] = "AssignmentStatement";
-    GuuASTNodeType["PRINT"] = "PrintStatement";
-    GuuASTNodeType["PROCEDURE_DECLARATION"] = "ProcedureDeclaration";
-})(GuuASTNodeType = exports.GuuASTNodeType || (exports.GuuASTNodeType = {}));
